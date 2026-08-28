7feaac266fec95700af973069ed8b2571e75fcbc
zulfaeuis1326 <zulfaeuis1326@gmail.com>
2026-08-28 13:19:52 +0800

    Add files via upload

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,8 +10,9 @@ export default async function DashboardLayout({
 }) {
   const supabase = createClient();
   const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
 
   if (!user) redirect("/login");
 
